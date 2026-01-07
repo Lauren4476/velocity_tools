4a069cd23489c50ac0ae8fdd4d9c167a96b56e8f
Lauren Mason <lmason@mpe.mpg.de>
2026-01-07 16:57:02 +0100

    fix unit conversion issue in G constant

--- src/velocity_tools/helper_functions.py
+++ src/velocity_tools/helper_functions.py
@@ -3,7 +3,7 @@ This file contains helper functions for the larger functions in velocity-tools.
 '''
 
 # Constants
-G = 6.67430e-11 * (1e-3)**2 * (1.4959787e-11) * (1.988416e30) # in au (km/s)^2 * Msol^-1
+G = 6.67430e-11 * (1e-3)**2 * (1.988416e30) / (1.4959787e11) # in au (km/s)^2 * Msol^-1
 au_in_km = 1.4959787e8 #km
 
 def au_to_m(quantity):
