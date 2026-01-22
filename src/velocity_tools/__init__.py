bc2400c1d807038e74c190e467ba65bba049631b
Lauren Mason <lmason@mpe.mpg.de>
2026-01-22 17:41:45 +0100

    add gradient_descent file

--- src/velocity_tools/__init__.py
+++ src/velocity_tools/__init__.py
@@ -4,5 +4,6 @@ from . import keplerian_field
 from . import stream_lines
 from .streamfit import stream_lines_grad
 from .streamfit import extract_streamline
+from .streamfit import gradient_descent
 
 from ._version import __version__
